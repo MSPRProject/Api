594eb383ab0f1f5f6d9afdffa9500e41d72acc30
kvnmri <kvn.mri.dev@gmail.com>
2025-04-10 10:51:55 +0200

    fix: rename from 'mspr' to 'mspr_api'

--- src/test/java/fr/mspr_api/MsprAPIApplicationTests.java
+++ src/test/java/fr/mspr_api/MsprAPIApplicationTests.java
@@ -1,10 +1,10 @@
-package fr.mspr;
+package fr.mspr_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MsprApplicationTests {
+class MsprAPIApplicationTests {
 
 	@Test
 	void contextLoads() {
