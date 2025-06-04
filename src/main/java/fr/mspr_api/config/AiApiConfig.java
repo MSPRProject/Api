8a914b19f0332bfbb1387826726b99b2d80b8e26
0Killian <49152982+0Killian@users.noreply.github.com>
2025-06-04 09:00:29 +0200

    17 ai api (#18)
    * feat: ai api config
    
    * feat: ai prompt method stub
    
    * fix: country iso3
    
    * feat: training data service
    
    * feat: get training data endpoint
    
    * feat: predict endpoint + cleanup
    
    * fix: missing header
    
    * fix: check if predicted report exists
    
    ---------
    
    Co-authored-by: kbellouard <killian.bellouard@getnobullshit.com>

---
+++ src/main/java/fr/mspr_api/config/AiApiConfig.java
@@ -0,0 +1,43 @@
+package fr.mspr_api.config;
+
+import jakarta.validation.constraints.NotBlank;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.validation.annotation.Validated;
+
+@ConfigurationProperties(prefix = "ai")
+@ConfigurationPropertiesScan
+@Validated
+public class AiApiConfig {
+
+    @NotBlank
+    private String apiKey;
+
+    @NotBlank
+    private String apiUrlStr;
+
+    private URI apiUri;
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getApiUrl() {
+        return apiUrlStr;
+    }
+
+    public void setApiUrl(String apiUrl) throws URISyntaxException {
+        this.apiUrlStr = apiUrl;
+        this.apiUri = new URI(apiUrl);
+    }
+
+    public URI getPromptUrl() {
+        return this.apiUri.resolve("/predict");
+    }
+}
